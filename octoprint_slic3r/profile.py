a6ff00dc51d016e8b577183113b63f36a8ae6ad1
ra100 <ra100x@gmail.com>
2016-10-01 14:14:25 +0200

    Remove unused import

--- octoprint_slic3r/profile.py
+++ octoprint_slic3r/profile.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 from __future__ import absolute_import
-import multiprocessing
 
 __author__ = "Gina Häußge <osd@foosel.net>, Javier Martínez Arrieta <martinezarrietajavier@gmail.com>"
 __license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
